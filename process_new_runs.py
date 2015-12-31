73418030df965606812e8a1c6aa3d3af22432ecb
lg9 <lg@u.washington.edu>
2015-12-31 13:31:40 -0800

    Working step1 (move files)
    Happy (for now) with def move_fastq_files()

--- process_new_runs.py
+++ process_new_runs.py
@@ -7,33 +7,35 @@ import os, re
 from subprocess import call
 
 # PARAMETERS and CONSTANTS -----------------------------------------------------
+home_dir = r'/home/lg/'
 storage_dir = r'/home/manoil-data/lg/Tn-seq_Ab/'
-working_dir = r'~/work_Ab/'
+working_dir = r'../work_Ab/'
 # ------------------------------------------------------------------------------
 
 def move_fastq_files():
+    cwd = os.getcwd()
+    os.chdir(home_dir)
     dirlist = os.listdir('.')
     dirpatt = re.compile(r'^\d*-?\d+_S\d+')
     fqfilepatt = re.compile(r'^\d*_?(.+)_S\d+.*((\.fastq)|(\.fq))(\.gz)?')
-    for item in dirlist:
-        if (os.path.isdir(item) and dirpatt.match(item)):
-            # it is a directory of the expected name; process it.
-            print "Copying directory " + item + " to storage location..."
-            args = ['cp', '-r', item + '/', storage_dir]
-            # exception handle the next command to cover if already copied
-            #call(args)
+    for rundir in dirlist:
+        if (os.path.isdir(rundir) and dirpatt.match(rundir)):
+            # it is a directory with name of the expected pattern; process it.
             dirname = None
-            m = re.match(r'^(\d{6}-\d+)_S\d', item)
+            m = re.match(r'^(\d{6}-\d+)_S\d', rundir)
             if m:
                 dirname = m.group(1)
             else:
-                m = re.match(r'^(\d{6})_S\d', item)
+                m = re.match(r'^(\d{6})_S\d', rundir)
                 if m:
                     dirname = m.group(1)
             if not dirname:
-                print("Could not parse directory", item)
+                print "Could not parse directory name, " + rundir
                 continue
-            os.chdir(item)
+            print "Copying directory " + rundir + " to storage location..."
+            args = ['cp', '-r', '-u', rundir + '/', storage_dir]
+            call(args)
+            os.chdir(rundir)
             itemdirlist = os.listdir('.')
             for item in itemdirlist:
                 m = re.search(fqfilepatt, item)
@@ -41,8 +43,13 @@ def move_fastq_files():
                     sample = m.group(1)
                     extension = m.group(2) + m.group(5)
                     newname = dirname + '_' + sample + extension
-                
-                    print "mv " + item + " " + working_dir + newname
-                    
-            os.chdir("..")
-            
+                    destination = working_dir + newname
+                    print "   executing command: mv " \
+                          + item + " " + destination
+                    args = ['mv', item, destination]
+                    call(args)
+            os.chdir(home_dir)
+            args = ['rmdir', rundir + r'/']
+            call(args)  # will see error if dir is not empty
+            # perhaps add exception handling for this in the future
+    os.chdir(cwd)
